839e672ea560792ebd3ac7b026c7cac31ff122e8
Himanshu <160750938+itshimanshu16@users.noreply.github.com>
2025-06-24 14:49:27 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ed-udaan-1.onrender.com/api", // Your backend base URL
+  baseURL: "https://ed-udaan.onrender.com/api", // Your backend base URL
 });
 
 // Register API
